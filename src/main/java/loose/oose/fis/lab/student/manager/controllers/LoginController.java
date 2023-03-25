943abd21a2ef629db2809e5782c7dd824e721b1a
peleemanuel <peleemy@gmail.com>
2023-03-25 21:31:57 +0200

    SM-1 Solved the comments. You can now view the students after you login as a teacher

--- src/main/java/loose/oose/fis/lab/student/manager/controllers/LoginController.java
+++ src/main/java/loose/oose/fis/lab/student/manager/controllers/LoginController.java
@@ -1,8 +1,14 @@
 package loose.oose.fis.lab.student.manager.controllers;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -28,9 +34,17 @@ public class LoginController {
             loginMessage.setText("Logged in as student!");
             return;
         }
-        if (username.equals("teacher") &&
-                password.equals("teacher")) {
-            loginMessage.setText("Logged in as teacher!");
+        if (username.equals("teacher") && password.equals("teacher")) {
+            try {
+                Stage stage = (Stage) loginMessage.getScene().getWindow();
+                Parent viewStudentsRoot =
+                        FXMLLoader.load(getClass().getResource("/loose/oose/fis/lab/student/manager/fxml/view-students.fxml"));
+
+                Scene scene = new Scene(viewStudentsRoot, 600, 400);
+                stage.setScene(scene);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return;
         }
         loginMessage.setText("Incorrect login!");
